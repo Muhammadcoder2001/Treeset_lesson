3899a633b654d7c1d0871b59ab2a756477c81539
Muhammadcoder2001 <abduqahhorovmuhammadqobil@gmail.com>
2024-08-06 10:45:37 +0500

    TAsk3

---
+++ TAsk3.java
@@ -0,0 +1,25 @@
+package TreeSet_lesson;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeSet;
+
+public class TAsk3 {
+
+    public static void main(String[] args) {
+
+        TreeSet<String> list = new TreeSet<>();
+        TreeSet <String> colors = new TreeSet<>();
+        list.add("White");
+        list.add("Pink");
+        list.add("Black");
+        list.add("Brown");
+        colors.addAll(list);
+        System.out.println(colors);
+        Iterator<String> color = colors.iterator();
+        while(color.hasNext()){
+            System.out.println(color.next());
+        }
+    }
+}
